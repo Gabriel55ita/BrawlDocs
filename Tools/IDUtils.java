dc82ab984d71ee6ee9af05aa5dc2d26b844986b0
Gabriel55ita <60621266+Gabriel55ita@users.noreply.github.com>
2022-07-07 23:18:02 +0200

    Tools

---
+++ Tools/IDUtils.java
@@ -0,0 +1,23 @@
+package tools;
+
+public class IDUtils {
+
+    // Characters for the Tag
+    private static final String[] tagChars = {"0", "2", "8", "9", "P", "Y", "L", "Q", "G", "R", "J", "C", "U", "V"};
+
+    /**
+     * Converts the id to the tag version
+     * @return Tag
+     */
+    public static String id2Tag(int id) {
+        StringBuilder tag = new StringBuilder();
+        while (id > 0) {
+            int charIndex = (int) Math.floor(id % tagChars.length);
+            tag.insert(0, tagChars[charIndex]); // appends the character at the beginning of the string.
+            id -= charIndex;
+            id /= tagChars.length;
+        }
+        return "#" + tag;
+    }
+
+}
